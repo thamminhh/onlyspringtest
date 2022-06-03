8642cc55ac975920da293bab980773879a91895e
thamminh <thamminh512@gmail.com>
2022-06-03 23:58:21 +0700

    first commit

---
+++ src/main/java/com/example/onlyspringtest/OnlyspringtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlyspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyspringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyspringtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlyspringtest/OnlyspringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlyspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyspringtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
